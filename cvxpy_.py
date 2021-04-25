4b8114423a3fd3f17d62033bb86c0f6f50891f2b
Tao Hong <76576903+Junyingli510@users.noreply.github.com>
2021-04-26 02:39:57 +0800

    Delete cvxpy_.py

--- cvxpy_.py
+++
@@ -1,9 +0,0 @@
-# import lib
-import cvxpy as cp
-import matplotlib.pyplot as plt
-import numpy as np
-
-# set initial data
-price_a = np.full(12, 325.)
-price_b = np.array([300, 300, 290, 275, 275, 280, 260, 250, 230, 200, 210, 190.])
-price_c = np.array([100, 110, 98, 115, 200, 220, 210, 500, 500, 490, 487, 550.])
